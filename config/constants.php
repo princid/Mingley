6426e093619be2613c6b9e3fbd2e1b86950af037
princid <princid85@gmail.com>
2023-11-02 10:33:49 +0530

    added the constants of DB

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,10 @@
 <?php
 
+define('SERVER_NAME', 'localhost');
+define('USER_NAME', 'phpmyadmin');
+define('USER_PASSWORD', 'root');
+define('DB_NAME', 'mingley');
+
 define("BASE_URL", "http://localhost/PHP_Assesments/Mingley/");
 
 // echo "Prince";
