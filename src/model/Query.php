5c26f70532046535bf6f9e9075e4491f41def82e
princid <princid85@gmail.com>
2023-11-03 18:32:02 +0530

    added function to fetch and update user

--- src/model/Query.php
+++ src/model/Query.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 require("../../config/connectDB.php");
 
@@ -25,20 +26,55 @@ function signUp($conn, $table, $first_name, $last_name, $user_name, $user_email,
 // 
 function signIn($conn, $table, $user_email, $user_password)
 {
-    $signIn_check = "SELECT user_email, user_password FROM $table WHERE user_email = '$user_email' AND user_password = '$user_password' ";
+    $signIn_check = "SELECT id, user_email, user_password FROM $table WHERE user_email = '$user_email' AND user_password = '$user_password' ";
     // $stmt_signIn_check = mysqli_prepare($conn, $signIn_check);
     $signIn_check_run = mysqli_query($conn, $signIn_check);
 
     if (mysqli_num_rows($signIn_check_run) > 0) {
         while($row = mysqli_fetch_assoc($signIn_check_run)) {
             $_SESSION["id"] = $row['id'];
+            // var_dump($row);
+            // exit;
             return "Logged In Successfully!";
         }
     }
     else{
         return "Error: OOPS! Something went wrong...";
     }
+}
 
+function fetchUserDetails($conn, $table, $condition){
+    $fetch_query = "SELECT * FROM $table WHERE $condition ";
+    $fetch_query_run = mysqli_query($conn, $fetch_query);
+
+    if($fetch_query_run && mysqli_num_rows($fetch_query_run) > 0) {
+        $row = mysqli_fetch_assoc($fetch_query_run);
+        // var_dump($row);
+        // exit;
+        $_SESSION['id'] = $row['id'];
+        return $row;
+    }
+}
+
+
+function updateProfile($conn, $table, $first_name, $last_name, $user_name, $user_email, $condition){
+
+    // $check_user = "SELECT id FROM $table WHERE user_email = '$user_email' OR user_name = '$user_name' ";
+    // $check_user_run = mysqli_query($conn, $check_user);
+
+    // if (mysqli_num_rows($check_user_run) > 0) {
+    //     // Email or Username already exists
+    //     return "Email/Username already exists! Try with another Email ID/Username.";
+    // } else {
+        $update_query = " UPDATE $table SET first_name = '$first_name', last_name = '$last_name', user_name = '$user_name', user_email = '$user_email' WHERE $condition ";
+        $update_query_run = mysqli_query($conn, $update_query);
+
+        if ($update_query_run) {
+            return "Your Data Updated Successfully!";
+        } else {
+            return "Error: " . mysqli_error($conn);
+        }
+    // }
     
 }
 
