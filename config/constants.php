6c51c052a67298f23cf7ed87d3cf56c8a182af83
princid <princid85@gmail.com>
2023-11-03 18:17:43 +0530

    added some important comments

--- config/constants.php
+++ config/constants.php
@@ -5,6 +5,8 @@ define('USER_NAME', 'phpmyadmin');
 define('USER_PASSWORD', 'root');
 define('DB_NAME', 'mingley');
 
+// For my laptop only
+
 // define('SERVER_NAME', 'localhost');
 // define('USER_NAME', 'root');
 // define('USER_PASSWORD', '');
