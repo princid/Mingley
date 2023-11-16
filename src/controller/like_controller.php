b333973aaac8149c0a9081c12f72f6bf4a84f087
amankumarsahani <amankumar.sahani096@gmail.com>
2023-11-16 15:06:47 +0530

    controller modified to count likes

--- src/controller/like_controller.php
+++ src/controller/like_controller.php
@@ -1,30 +1,21 @@
 <?php
-
-
-
-
-
 session_start();
 
 require_once("../model/Query.php");
 
 if (!empty($_SESSION)) {
-    
+
     $postID = $_POST['post_id'];
     $userID = $_SESSION['id'];
 
     $like_stat = toggleLike($conn, $postID, $userID);
 
-    require_once('show_post_on_feed.php');
+    // Likes Counter
+    $likes_table = 'likes_table';
+    $likesCounter = likesCounter($conn, $likes_table, $postID);
 
-    $count_like = $feed_post_result[0]['likes_count'];
-
-    $response = ["likes_count" => $count_like, "status" => 200, "like_status" => $like_stat, "post_id" => $postID];
-    // $response = ["status" => 200, "like_status" => $like_stat, "post_id" => $postID];
+    // Sending response to the ajax
+    $response = ["likes_count" => $likesCounter, "status" => 200, "like_status" => $like_stat, "post_id" => $postID];
 
     echo json_encode($response);
-
-    // echo $response;
 }
-
-?>
