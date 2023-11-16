84222d0437adff752ae50079b0b3efa24afe78be
amankumarsahani <amankumar.sahani096@gmail.com>
2023-11-16 15:04:58 +0530

    ajax modified to change number of likes

--- assets/js/postLike.js
+++ assets/js/postLike.js
@@ -1,39 +1,52 @@
 $(document).ready(function () {
-  $(document).on("click", ".likesAnchor", function (event) {
-    event.preventDefault();
-    const postID = $(this).data("post-id");
-    const likeStatus = $(this).data("like-status");
-    const postLike = $(`.postLike[data-post-id="${postID}"]`);
+  let like_btn = document.querySelectorAll(".likesAnchor");
+  like_btn.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetElement = el;
+      const postID = el.dataset["postId"];
+      const likeStatus = el.dataset["likeStatus"];
+      const postLike = el.querySelector(`.postLike[data-post-id="${postID}"]`);
+      const likeCounter = el.querySelector("#likes_count_" + postID);
 
-    $.ajax({
-    //   url: "../controller/like_controller.php",
-      url: "../../src/controller/like_controller.php",
-      method: "POST",
-      data: {
-        post_id: postID,
-      },
-      dataType: "json",
-      success: function (response) {
-        console.log(response);
-        if (response["like_status"] === "liked") {
-          postLike
-            .removeClass("fa-regular")
-            .addClass("fa-solid")
-            .css("color", "red");
-          $(this).data("like-status", 1);
-        } else if (response["like_status"] === "unliked") {
-          postLike
-            .removeClass("fa-solid")
-            .addClass("fa-regular")
-            .css("color", "initial");
-          $(this).data("like-status", 0);
-        }
-        var post_id = response["post_id"];
-        $("#likes_count_" + post_id).html(response["likes_count"]);
-      },
-      error: function (error) {
-        console.error("Error:", error);
-      },
+      $.ajax({
+        //   url: "../controller/like_controller.php",
+        url: "../../src/controller/like_controller.php",
+        method: "POST",
+        data: {
+          post_id: postID,
+          like_status: likeStatus,
+        },
+        dataType: "json",
+        success: function (response) {
+          if (response["like_status"] === "liked") {
+            likeCounter.innerHTML = response["likes_count"];
+            targetElement.dataset["likeStatus"] = 1;
+
+            // Changing Style of Like
+            postLike.classList.remove("fa-regular");
+            postLike.classList.add("fa-solid");
+            postLike.style.color = "red";
+            $(this).data("like-status", 1);
+          } else if (response["like_status"] === "unliked") {
+            likeCounter.innerHTML = response["likes_count"];
+            targetElement.dataset["likeStatus"] = 0;
+
+            // Changing Style of unlike
+            postLike.classList.remove("fa-solid");
+            postLike.classList.add("fa-regular");
+            postLike.style.color = "initial";
+            $(this).data("like-status", 0);
+          }
+          var post_id = response["post_id"];
+          $("#likes_count_" + post_id).html(response["likes_count"]);
+        },
+        error: function (xhr, status, error) {
+          console.error("AJAX Error:", status, error);
+          console.log("Response Text:", xhr.responseText);
+        },
+      });
     });
   });
+  $(document).on("click", ".likesAnchor", function (event) {});
 });
