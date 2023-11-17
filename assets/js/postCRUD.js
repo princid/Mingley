8b1b496b8c0bf2f4c14d697040760b7a0bd78ae6
amankumarsahani <amankumar.sahani096@gmail.com>
2023-11-17 18:09:45 +0530

    screen freeze fixed

--- assets/js/postCRUD.js
+++ assets/js/postCRUD.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
   delete_btns.forEach((el) => {
     el.addEventListener("click", (e) => {
       const postId = e.target.dataset["postId"];
+      console.log();
       $.ajax({
-        url: "http://localhost/PHP_Assesments/Mingley/src/controller/crud_post_controller.php",
+        url: "../../src/controller/crud_post_controller.php",
         method: "POST",
         data: {
           callHandler: "userPostDeleteHandler",
@@ -15,27 +16,28 @@ $(document).ready(function () {
         },
         dataType: "json",
         success: function (data) {
+          $(".btn-close").click();
+          $("#post_card"+postId).remove();
+
           if (data["status"] == 200) {
-            $(".modal").hide();
-            $(".modal-backdrop").hide();
-            // $("body").removeAttr("style");
             $("#msg").html(
-              `<div class="alert alert-success alert-dismissible fade show" role="alert">
-                <strong class="h1">${data["message"]}</strong>
+              `<div class="alertBox"><div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong class="">${data["message"]}</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>`
+              </div></div>`
             );
           } else {
-            $(".modal").hide();
-            $(".modal-backdrop").hide();
-            // $("body").removeAttr("style");
             $("#msg").html(
-              `<div class="alert alert-success alert-dismissible fade show" role="alert">
-                <strong class="h1">${data["message"]}</strong>
+              `<div class="alertBox"><div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong class="">${data["message"]}</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div>`
+              </div></div>`
             );
           }
+
+          setTimeout(() => {
+            $("#msg").html("");
+          }, 3000);
         },
         error: function (xhr, status, error) {
           console.error("AJAX Error:", status, error);
@@ -54,7 +56,7 @@ $(document).ready(function () {
       const postCaption = document.querySelector("#caption" + postId).value;
 
       $.ajax({
-        url: "http://localhost/PHP_Assesments/Mingley/src/controller/crud_post_controller.php",
+        url: "../../src/controller/crud_post_controller.php",
         method: "POST",
         data: {
           callHandler: "editPostHandler",
@@ -65,15 +67,21 @@ $(document).ready(function () {
         },
         dataType: "json",
         success: function (data) {
-          $(".modal").hide();
-          $(".modal-backdrop").hide();
+          $(".btn-close").click();
           $("#msg").html(
-            `<div class="alertBox"><div class="alert alert-success" role="alert"><h1>${data["message"]}</h1></div></div>`
+            `<div class="alertBox"><div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong class="">${data["message"]}</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div></div>`
           );
 
           if (data["status"] == 200) {
             $("#post_caption_" + postId).html(data["update_caption"]);
           }
+
+          setTimeout(() => {
+            $("#msg").html("");
+          }, 3000);
         },
         error: function (xhr, status, error) {
           console.error("AJAX Error:", status, error);
