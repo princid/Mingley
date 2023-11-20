2fafed8a52bc96bafc1e3c2760bbfb97fcd62ad5
princid <princid85@gmail.com>
2023-11-20 20:03:22 +0530

    added follow action controller

---
+++ src/controller/follow_action.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+require("../../config/connectDB.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['userId'])) {
+    $userId = $_POST['userId'];
+    $followerId = $_SESSION['id'];
+
+    // Check if the user is already being followed
+    $query = "SELECT * FROM follows_table WHERE user_id = $userId AND follower_id = $followerId";
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+        if (mysqli_num_rows($result) === 0) {
+            $insertQuery = "INSERT INTO follows_table (user_id, follower_id, follow_status, created_at) VALUES ($userId, $followerId, 1, NOW())";
+            if (mysqli_query($conn, $insertQuery)) {
+                // Successfully followed
+                echo json_encode(['status' => 'success', 'message' => 'User followed', 'follow_status' => 1]);
+            } else {
+                // Error in database operation
+                echo json_encode(['status' => 'error', 'message' => 'Failed to follow user']);
+            }
+        } else {
+            // If already following, update the follow status
+            $updateQuery = "UPDATE follows_table SET follow_status = 1 - follow_status WHERE user_id = $userId AND follower_id = $followerId";
+            if (mysqli_query($conn, $updateQuery)) {
+                // Successfully updated follow status
+
+                // Fetch the updated follow status from the database
+                $updatedStatusQuery = "SELECT follow_status FROM follows_table WHERE user_id = $userId AND follower_id = $followerId";
+                $updatedStatusResult = mysqli_query($conn, $updatedStatusQuery);
+                $updatedStatus = mysqli_fetch_assoc($updatedStatusResult)['follow_status'];
+
+                echo json_encode(['status' => 'success', 'message' => 'Follow status updated', 'follow_status' => $updatedStatus]);
+            } else {
+                // Error in database operation
+                echo json_encode(['status' => 'error', 'message' => 'Failed to update follow status']);
+            }
+        }
+    } else {
+        // Error in database operation
+        echo json_encode(['status' => 'error', 'message' => 'Database error']);
+    }
+} else {
+    // Invalid request method or missing parameters
+    echo json_encode(['status' => 'error', 'message' => 'Invalid request']);
+}
