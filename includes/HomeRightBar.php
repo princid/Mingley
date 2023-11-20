248e2bfda925335e8baba586f67b94d3a95c4575
princid <princid85@gmail.com>
2023-11-20 20:01:14 +0530

    Separated the Right bar of Home page

---
+++ includes/HomeRightBar.php
@@ -0,0 +1,76 @@
+
+<!-- Right Bar starts here -->
+<div class="col-lg-3">
+    <div class="row g-4">
+
+
+        <!-- Card News START -->
+        <div class="col-sm-6 col-lg-12">
+            <div class="card rounded-2">
+                <!-- Card header START -->
+                <div class="card-header pb-0 border-0">
+                    <h5 class="card-title mb-0">Today’s news</h5>
+                </div>
+                <!-- Card header END -->
+                <!-- Card body START -->
+                <div class="card-body">
+                    <!-- News item -->
+                    <div class="mb-3">
+                        <h6 class="mb-0"><a href="">Ten questions you should answer truthfully</a></h6>
+                        <small>2hr</small>
+                    </div>
+                    <!-- News item -->
+                    <div class="mb-3">
+                        <h6 class="mb-0"><a href="">Five unbelievable facts about money</a></h6>
+                        <small>3hr</small>
+                    </div>
+                    <!-- News item -->
+                    <div class="mb-3">
+                        <h6 class="mb-0"><a href="">Best Pinterest Boards for learning about business</a></h6>
+                        <small>4hr</small>
+                    </div>
+                    <!-- News item -->
+                    <div class="mb-3">
+                        <h6 class="mb-0"><a href="">Skills that you can learn from business</a></h6>
+                        <small>6hr</small>
+                    </div>
+                    <!-- Load more comments -->
+                    <a href="#!" role="button" class="btn btn-link btn-link-loader btn-sm text-secondary d-flex align-items-center active" data-bs-toggle="button" aria-pressed="true">
+                        <div class="spinner-dots me-2">
+                            <span class="spinner-dot"></span>
+                            <span class="spinner-dot"></span>
+                            <span class="spinner-dot"></span>
+                        </div>
+                        View all latest news
+                    </a>
+                </div>
+                <!-- Card body END -->
+            </div>
+        </div>
+        <!-- Card News END -->
+
+        <ul class="nav small mt-4 justify-content-center lh-1">
+            <li class="nav-item">
+                <a class="nav-link" href="">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Settings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" target="_blank" href="">Support </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" target="_blank" href="">Docs </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Help</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Privacy &amp; terms</a>
+            </li>
+        </ul>
+
+        <p class="small text-center mt-1">©2023 <a class="text-reset" target="_blank" href=""> Mingley </a></p>
+    </div>
+</div>
+<!-- Right Bar ends here -->
