8df7d35acfb8fb858029ac7ac56125abb210350e
princid <princid85@gmail.com>
2023-11-23 19:17:16 +0530

    added the favicon

--- includes/Header.php
+++ includes/Header.php
@@ -16,6 +16,7 @@ session_start();
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" type="image" href="../../assets/img/logo2.png">
     <!-- <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/bootstrap/icons/bootstrap-icons.css"> -->
 
