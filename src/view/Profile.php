c71d3cd9d3b7d137052c4f1c82d1908b8289aa47
princid <princid85@gmail.com>
2023-11-24 19:06:11 +0530

    added styling to image preview title

--- src/view/Profile.php
+++ src/view/Profile.php
@@ -327,9 +327,8 @@ $receiver = $_GET['user_id'];
                                             </div>
 
                                             <div id="preview" class="p-3">
-                                                <h3>Image Preview</h3>
+                                                <h3 class="text-center">Image Preview</h3>
                                                 <div id="image_preview">
-
                                                 </div>
                                             </div>
 
@@ -638,7 +637,7 @@ $receiver = $_GET['user_id'];
                                                             $comment_owner_name        = $get_comment_data['first_name'] . " " . $get_comment_data['last_name'];
                                                             $comment_owner_username    = $get_comment_data['user_name'];
                                                             $comment_text              = $get_comment_data['comment_text'];
-                                                            $comment_time              = $get_comment_data['comment_date'];
+                                                            $comment_time              = $get_comment_data['comment_date'] . " - " . $get_comment_data['comment_time'];
 
                                                         ?>
 
@@ -916,7 +915,7 @@ $receiver = $_GET['user_id'];
     $(document).ready(function() {
         const follower_table = $('#follower_table_id').DataTable({
             "paging": true,
-            "pageLength": 2,
+            "pageLength": 10,
             "order": [1, 'asc'],
             "pagingType": "simple_numbers",
             "language": {
@@ -936,7 +935,7 @@ $receiver = $_GET['user_id'];
 
         const following_table = $('#following_table_id').DataTable({
             "paging": true,
-            "pageLength": 2,
+            "pageLength": 10,
             "order": [1, 'asc'],
             "pagingType": "simple_numbers",
             "language": {
@@ -953,127 +952,12 @@ $receiver = $_GET['user_id'];
                 "searchPlaceholder": "Search..."
             }
         });
-
-        // Post preview
-        const postInputBtn = document.getElementById("upload_file");
-
-        postInputBtn.addEventListener("change", function(el) {
-            $("#image_preview").empty();
-
-            const postPreview = document.querySelector("#image_preview");
-            const totalFiles = postInputBtn["files"].length;
-            if (totalFiles > 0) {
-                $("#preview").show();
-
-                $("#image_preview").append(`
-  <div id="carouselExampleIndicators" class="carousel carousel-dark slide">
-    <div class="carousel-indicators">
-    ${(() => {
-        let indicators = '';
-        for (let i = 0; i < totalFiles; i++) {
-          indicators += `
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${i}" ${i === 0 ? 'class="active" aria-current="true"' : ''} aria-label="Slide ${i + 1}"></button>
-          `;
-        }
-        return indicators;
-      })()}
-
-    </div>
-    <div class="carousel-inner">
-      ${(() => {
-        let carouselItems = '';
-
-        for (let i = 0; i < totalFiles; i++) {
-
-          carouselItems += `
-            <div class="carousel-item preview-post ${i === 0 ? 'active' : ''}">
-              <img src="${URL.createObjectURL(postInputBtn.files[i])}" class="d-block h-100 object-fit-contain w-100" alt="...">
-            </div>
-          `;
-        }
-
-        return carouselItems;
-      })()}
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-`);
-            }
-        });
-
-        // Reset form
-        var modalCloseButton = $('[data-bs-dismiss="modal"]');
-
-        modalCloseButton.on('click', function() {
-            $("#image_preview").empty();
-            $("#preview").hide();
-            $("#submit_post")[0].reset();
-
-        });
-
-        // Profile image preview
-        const fileInput = document.getElementById("profile_pic");
-        fileInput.addEventListener("change", function(el) {
-            const profileImage = document.querySelector("#preview-avatar");
-            console.log(profileImage);
-            if (fileInput["files"].length > 0) {
-                profileImage.src = URL.createObjectURL(fileInput.files[0]);
-            }
-        });
-
-
-        $(".removeBtn").on('click', function(e) {
-            const userId = e.target.dataset["userId"];
-            $.ajax({
-                url: "../../src/controller/crud_profile_controller.php",
-                method: "POST",
-                data: {
-                    callHandler: "deleteProfileImage",
-                    dataset: {
-                        user_id: userId,
-                    },
-                },
-                dataType: "json",
-                success: function(data) {
-                    $(".avatar-img").attr("src", "<?= BASE_URL ?>assets/profile_pic/profileDummy.png");
-                    $(".btn-close").click();
-
-                    if (data["status"] == 200) {
-                        $("#msg").html(
-                            `<div class="alertBox"><div class="alert alert-success alert-dismissible fade show" role="alert">
-                <strong class="">${data["message"]}</strong>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div></div>`
-                        );
-                    } else {
-                        $("#msg").html(
-                            `<div class="alertBox"><div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <strong class="">${data["message"]}</strong>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-              </div></div>`
-                        );
-                    }
-
-                    setTimeout(() => {
-                        $("#msg").html("");
-                    }, 5000);
-                },
-                error: function(xhr, status, error) {
-                    console.error("AJAX Error:", status, error);
-                },
-            });
-        })
     });
 </script>
 
 <script src="../../assets/js/postComment.js"></script>
+
+<script src="../../assets/js/postPreview.js"></script>
 
 <?php
 require_once("../../includes/Footer.php");
