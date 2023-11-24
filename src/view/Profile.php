c0fcc6cffdff3cae6edecae425f7c9ce77908972
amankumarsahani <amankumar.sahani096@gmail.com>
2023-11-24 13:23:57 +0530

    preview post images functionality added

--- src/view/Profile.php
+++ src/view/Profile.php
@@ -328,7 +328,9 @@ $receiver = $_GET['user_id'];
 
                                             <div id="preview" class="p-3">
                                                 <h3>Image Preview</h3>
-                                                <div id="image_preview"></div>
+                                                <div id="image_preview">
+
+                                                </div>
                                             </div>
 
                                             <div class="modal-footer">
@@ -952,6 +954,70 @@ $receiver = $_GET['user_id'];
             }
         });
 
+        // Post preview
+        const postInputBtn = document.getElementById("upload_file");
+
+        postInputBtn.addEventListener("change", function(el) {
+            $("#image_preview").empty();
+
+            const postPreview = document.querySelector("#image_preview");
+            const totalFiles = postInputBtn["files"].length;
+            if (totalFiles > 0) {
+                $("#preview").show();
+
+                $("#image_preview").append(`
+  <div id="carouselExampleIndicators" class="carousel carousel-dark slide">
+    <div class="carousel-indicators">
+    ${(() => {
+        let indicators = '';
+        for (let i = 0; i < totalFiles; i++) {
+          indicators += `
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${i}" ${i === 0 ? 'class="active" aria-current="true"' : ''} aria-label="Slide ${i + 1}"></button>
+          `;
+        }
+        return indicators;
+      })()}
+
+    </div>
+    <div class="carousel-inner">
+      ${(() => {
+        let carouselItems = '';
+
+        for (let i = 0; i < totalFiles; i++) {
+
+          carouselItems += `
+            <div class="carousel-item preview-post ${i === 0 ? 'active' : ''}">
+              <img src="${URL.createObjectURL(postInputBtn.files[i])}" class="d-block h-100 object-fit-contain w-100" alt="...">
+            </div>
+          `;
+        }
+
+        return carouselItems;
+      })()}
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+`);
+            }
+        });
+
+        // Reset form
+        var modalCloseButton = $('[data-bs-dismiss="modal"]');
+
+        modalCloseButton.on('click', function() {
+            $("#image_preview").empty();
+            $("#preview").hide();
+            $("#submit_post")[0].reset();
+
+        });
+
         // Profile image preview
         const fileInput = document.getElementById("profile_pic");
         fileInput.addEventListener("change", function(el) {
@@ -976,7 +1042,7 @@ $receiver = $_GET['user_id'];
                 },
                 dataType: "json",
                 success: function(data) {
-                    $(".avatar-img").attr("src", "<?=BASE_URL?>assets/profile_pic/profileDummy.png");
+                    $(".avatar-img").attr("src", "<?= BASE_URL ?>assets/profile_pic/profileDummy.png");
                     $(".btn-close").click();
 
                     if (data["status"] == 200) {
