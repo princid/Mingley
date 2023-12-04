762daf7bef310e65a2beae3edc57c17f0d4e97bf
princid <princid85@gmail.com>
2023-12-04 18:49:39 +0530

    added function to insert notification

---
+++ src/controller/insert_notification.php
@@ -0,0 +1,12 @@
+<!-- <?php
+
+// session_start();
+// require_once("../model/Query.php");
+
+// function handleNotification($conn, $receiver_id, $sender_id, $notif_type, $notif_content, $postId)
+// {
+//     // Call the model function to insert the notification
+//     insertNotification($conn, $receiver_id, $sender_id, $notif_type, $notif_content, $postId);
+// }
+
+?> -->
