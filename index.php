9934d057aa1b2387ddeabb74dfd28ec89398fe06
princid <princid85@gmail.com>
2023-12-12 19:56:08 +0530

    styled the index page

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
-<?php
+<!DOCTYPE html>
 
-// include("src/view/SignUp.php");
+<html>
 
-include("includes/Header.php");
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Mingley</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image" href="assets/img/logo2.png">
+
+    <!-- Bootstrap CSS CDN Link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- Font Awesome CDN Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+    <div style="margin-top: 20%;">
+        <h1 class="text-center">Welcome to <strong class="brand_name">Mingley</strong></h1>
+        <br>
+        <a href="src/view/SignUp.php" style="display: block; text-align: center; font-size: 25px; text-decoration:none;">Click here to Explore Mingley</a>
+    </div>
 
 
-?>
+    <!-- Bootstrap JS CDN Link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-<h1>Welcome to <span class="text-success">Mingley</span></h1>
-<br>
-<a href="src/view/SignUp.php">Go to Sign Up Page</a>
+
+
+
+</body>
+
+</html>
