b6f11be3e1b7c6800351f99d17de212ad6ace61d
princid <princid85@gmail.com>
2023-12-14 18:44:05 +0530

    added line breaks in messages

--- response/chat_loader.php
+++ response/chat_loader.php
@@ -15,20 +15,21 @@ $chatArea_result = mysqli_query($conn, $chatArea_query);
 if ($chatArea_result) {
     foreach ($chatArea_result as $msg) {
 
-        $chatUser_id = $msg["id"];
-        $show_msg    = $msg["text_message"];
-        $msg_time    = $msg["curr_date"] . " - " . $msg["curr_time"];
-        $profile_pic = $msg["user_profile_pic"];
+        $chatUser_id   = $msg["id"];
+        $chatUser_owner = $msg['user_name'];
+        $show_msg      = nl2br($msg["text_message"]);
+        $msg_time      = $msg["curr_date"] . " - " . $msg["curr_time"];
+        $profile_pic   = $msg["user_profile_pic"];
 
         if ($sender == $msg["id"]) {
 ?>
 
             <div class="item-group-you d-flex">
                 <?php if (!empty($profile_pic)) { ?>
-                    <img class="avatar-img rounded-circle" src="<?= BASE_URL ?>assets/profile_pic/<?= $chatUser_id . "/" . $profile_pic; ?>" alt="" style="width: 3rem; height: 3rem;">
+                    <img title="<?= $chatUser_owner; ?>" class="avatar-img rounded-circle" src="<?= BASE_URL ?>assets/profile_pic/<?= $chatUser_id . "/" . $profile_pic; ?>" alt="" style="width: 3rem; height: 3rem;">
 
                 <?php } else { ?>
-                    <img class="avatar-img rounded-circle" src="<?= BASE_URL ?>assets/profile_pic/profileDummy.png" alt="" style="width: 3rem;">
+                    <img title="<?= $chatUser_owner; ?>" class="avatar-img rounded-circle" src="<?= BASE_URL ?>assets/profile_pic/profileDummy.png" alt="" style="width: 3rem;">
 
                 <?php } ?>
                 <div class="text-message-you">
@@ -41,18 +42,18 @@ if ($chatArea_result) {
         <?php } else { ?>
             <div class=" item-group-other d-flex">
                 <?php if (!empty($profile_pic)) { ?>
-                    <img class="avatar-img rounded-circle" src="<?= BASE_URL ?>assets/profile_pic/<?= $chatUser_id . "/" . $profile_pic; ?>" alt="" style="width: 3rem; height: 3rem;">
+                    <img title="<?= $chatUser_owner; ?>" class="avatar-img rounded-circle" src="<?= BASE_URL ?>assets/profile_pic/<?= $chatUser_id . "/" . $profile_pic; ?>" alt="" style="width: 3rem; height: 3rem;">
 
                 <?php } else { ?>
-                    <img class="avatar-img rounded-circle" src="<?= BASE_URL ?>assets/profile_pic/profileDummy.png" alt="" style="width: 3rem;">
+                    <img title="<?= $chatUser_owner; ?>" class="avatar-img rounded-circle" src="<?= BASE_URL ?>assets/profile_pic/profileDummy.png" alt="" style="width: 3rem;">
 
-                <?php } ?>
-                <div class="text-message-other">
-                    <?php echo $show_msg; ?>
-                </div>
-                <p class="time-track-other">
-                    <?php echo $msg_time ?>
-                </p>
+                    <?php } ?>
+                    <div class="text-message-other">
+                        <?php echo $show_msg; ?>
+                    </div>
+                    <p class="time-track-other">
+                        <?php echo $msg_time ?>
+                    </p>
             </div>
 
 <?php }
